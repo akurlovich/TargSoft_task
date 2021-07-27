bae080ae0cc62f249a0225a39cb81a071f896d49
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-07-27 14:00:04 +0300

    init: targsoft task

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './css/main.css';
+import './scss/main.scss';
+import './index.html'
+
+import App from './App';
+
+
+ReactDOM.render(
+    <App/>,
+  document.getElementById('root')
+);
