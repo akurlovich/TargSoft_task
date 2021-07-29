b12586d2d5c78282d707929f502f8ab6de4cdcf6
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-07-29 08:33:20 +0300

    feat: collums

--- src/ts/components/FluentUIList.tsx
+++ src/ts/components/FluentUIList.tsx
@@ -20,95 +20,87 @@ const FluentUI: FC = () => {
     return <h1 style={{fontSize: '60px'}}>{error}</h1>
   }
 
+  const arr = [
+    {
+      name: '1111',
+      data: 123,
+      last: 'wwww',
+      email: 'rrrr',
+    },
+    {
+      name: '2222',
+      data: 2123,
+      last: 'wwww2',
+      email: 'rrrr2',
+    },
+    {
+      name: '3333',
+      data: 3123,
+      last: 'wwww3',
+      email: 'rrrr3',
+    },
+  ]
+
   const columns: IColumn[] = [
     {
       key: 'column1',
       name: 'File Type',
-      // className: classNames.fileIconCell,
-      // iconClassName: classNames.fileIconHeaderIcon,
-      ariaLabel: 'Column operations for File type, Press to sort on File type',
-      // iconName: 'Page',
-      // isIconOnly: true,
-      fieldName: 'name',
+      fieldName: 'userId',
+      minWidth: 16,
+      maxWidth: 16,
+      isResizable: true,
+      isCollapsible: true,
+    },
+    {
+      key: 'column2',
+      name: 'File Type2',
+      fieldName: 'id',
       minWidth: 16,
       maxWidth: 16,
     },
     {
-      key: 'column2',
-      name: 'Name',
-      fieldName: 'name',
-      minWidth: 210,
-      maxWidth: 350,
-      isRowHeader: true,
-      isResizable: true,
-      isSorted: true,
-      isSortedDescending: false,
-      sortAscendingAriaLabel: 'Sorted A to Z',
-      sortDescendingAriaLabel: 'Sorted Z to A',
-      // onColumnClick: this._onColumnClick,
-      data: 'string',
-      isPadded: true,
-    },
-    {
       key: 'column3',
-      name: 'Date Modified',
-      fieldName: 'dateModifiedValue',
-      minWidth: 70,
-      maxWidth: 90,
-      isResizable: true,
-      // onColumnClick: this._onColumnClick,
-      data: 'number',
-      // onRender: (item: IDocument) => {
-      //   return <span>{item.dateModified}</span>;
-      // },
-      isPadded: true,
+      name: 'File Type3',
+      fieldName: 'title',
+      minWidth: 16,
+      maxWidth: 16,
     },
     {
       key: 'column4',
-      name: 'Modified By',
-      fieldName: 'modifiedBy',
-      minWidth: 70,
-      maxWidth: 90,
-      isResizable: true,
-      isCollapsible: true,
-      data: 'string',
-      // onColumnClick: this._onColumnClick,
-      // onRender: (item: IDocument) => {
-      //   return <span>{item.modifiedBy}</span>;
-      // },
-      isPadded: true,
+      name: 'File Type4',
+      fieldName: 'body',
+      minWidth: 16,
+      maxWidth: 16,
     },
-    {
-      key: 'column5',
-      name: 'File Size',
-      fieldName: 'fileSizeRaw',
-      minWidth: 70,
-      maxWidth: 90,
-      isResizable: true,
-      isCollapsible: true,
-      data: 'number',
+    // {
+      // key: 'column5',
+      // name: 'File Size',
+      // fieldName: 'fileSizeRaw',
+      // minWidth: 70,
+      // maxWidth: 90,
+      // isResizable: true,
+      // isCollapsible: true,
+      // data: 'number',
       // onColumnClick: this._onColumnClick,
       // onRender: (item: IDocument) => {
       //   return <span>{item.fileSize}</span>;
       // },
-    }
+    // }
   ]
 
   return (
     <div>
-      {posts.map(user => 
-        <DetailsList
-          items={users}
-          // compact={isCompactMode}
-          columns={columns}
-          // selectionMode={SelectionMode.none}
-          // getKey={this._getKey}
-          // setKey="none"
-          // layoutMode={DetailsListLayoutMode.justified}
-          // isHeaderVisible={true}
-          // onItemInvoked={this._onItemInvoked}
-        />
-      )}
+      <DetailsList
+        items={posts}
+        // compact={isCompactMode}
+        columns={columns}
+        // selectionMode={SelectionMode.none}
+        // getKey={this._getKey}
+        // setKey="none"
+        // layoutMode={DetailsListLayoutMode.justified}
+        // isHeaderVisible={true}
+        // onItemInvoked={this._onItemInvoked}
+      />
       {/* {posts.map(user => 
         <div key={user.id}>{user.title}</div>  
       )} */}
